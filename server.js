5c5ddfa4862252bf1d270fc572fc4266119def6b
Michael Westley <mwestley8806@yahoo.com>
2018-03-04 14:15:21 -0500

    first push

---
+++ server.js
@@ -0,0 +1,89 @@
+var express = require("express");
+var bodyParser = require("body-parser");
+var logger = require("morgan");
+var mongoose = require("mongoose");
+
+var axios = require("axios");
+var cheerio = require("cheerio");
+
+var db = require ("./models");
+
+var PORT = 3000;
+
+var app = express();
+
+app.use(logger("dev"));
+
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.use(express.static("public"));
+
+mongoose.Promise = Promise;
+mongoose.connect("mongodb://localhost/week18Populater", {
+    // useMongoClient: true
+});
+
+app.get("/scrape", function(req, res) {
+    axios.get("http://www.clickorlando.com/").then(function(response) {
+        var $ = cheerio.load(response.data);
+
+        $("h3").each(function(i, element) {
+            var result = {};
+
+            result.title = $(this)
+                .children("a")
+                .text();
+            result.link = $(this)
+                .children("a")
+                .attr("href");
+
+            db.Article.create(result)
+                .then(function(dbArticle) {
+                    console.log(dbArticle);
+                })
+                .catch(function(err) {
+                    return res.json(err);
+                });
+        });
+
+        res.send("Scrape Complete");
+    });
+});
+
+app.get("/articles", function(req, res) {
+    db.Article.find({})
+        .then(function(dbArticle) {
+            res.json(dbArticle);
+        })
+        .catch(function(err) {
+            res.json(err);
+        });
+});
+
+app.get("/article/:id", function(req, res) {
+    db.Article.findOne({ _id: req.params.id })
+        .populate("note")
+        .then(function(dbArticle) {
+            res.json(dbArticle);
+        })
+        .catch(function(err) {
+            res.json(err)
+        });
+});
+
+app.post("/articles/:id", function(req, res) {
+    db.Note.create(req.body)
+        .then(function(dbNote) {
+            return db.Article.findOneAndUpdate({ _id:req.params.id}, { note: dbNote._,id}, { new: true });
+        })
+        .then(function(dbArticle) {
+            res.json(dbArticle);
+        })
+        .catch(function(err) {
+            res.json(err);
+        });
+});
+
+app.listen(PORT, function() {
+    console.log("App running on port " + PORT + "!");
+});
